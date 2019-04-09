63fa857d7a96796579cd17a8c32cc6984e2eb06a
LaraGastaldi <lara.f.gastaldi@gmail.com>
2019-04-09 14:21:23 -0300

    Add files via upload

---
+++ model/comment.class.php
@@ -0,0 +1,45 @@
+<?php
+namespace model;
+
+class Comments
+{
+	use database;
+
+	protected $database;
+	public $path;
+	public $userID;
+	public $reviewID;
+	public $comment;
+	public $date;
+	
+	function __construct()
+	{	
+		$this->path = null;
+		$this->userId = null;
+		$this->reviewId = null;
+		$this->comment = null;
+	}
+
+	function newComment($comment){
+		/*
+			Define the vars in object for manipulation in others functions
+		*/
+		$formdate = DateTime::createFromFormat('d/m/Y');
+		if($formdate && $formdate->format('d/m/Y') === $date){
+		   $this->date = $date;
+		}else{
+			$this->date = $formdate;
+		}
+	}
+
+
+	function setQuerie($querie, $position){
+		$this->QUERIES[$position] = $querie;
+	  }
+	
+	  public function executeQuerie($anyNinfo,$querie,$encoding){
+		$querie = $this->command($andNinfo,$this->$QUERIES[$querie],$encoding);
+	  }
+}
+
+
